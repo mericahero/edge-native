f321c362a951edf1306bcffa11849d851cd8e699
Chen Chen <chenchen@shopee.com>
2023-05-12 10:59:32 +0800

    first commit

---
+++ App.tsx
@@ -0,0 +1,40 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import FirstView from '@/pages/demo/first_view'
+
+
+function App(): JSX.Element {
+
+
+  return (
+    <FirstView />
+  );
+}
+
+
+export default App;
